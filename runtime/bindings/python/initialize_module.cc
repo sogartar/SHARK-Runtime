iree-org/iree

[runtime][python] Add debug sink to bindings
We don't support custom debug sinks in the Runtime Python bindings.
In particular the ability to register a custom callback when tracing
tensors.

This change makes it possible to create a HAL module with a Python
function as a callback.
A special care is taken to avoid leaking due to circular references
where the callback reference the HAL module itself.

Signed-off-by: Boian Petkantchin <boian.petkantchin@amd.com>

runtime/bindings/python/initialize_module.cc
@@ -4,7 +4,10 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <nanobind/intrusive/counter.h>
+
 #include <memory>
+#include <nanobind/intrusive/counter.inl>
 
 #include "./binding.h"
 #include "./hal.h"
@@ -78,6 +81,16 @@ NB_MODULE(_runtime, m) {
   });
 
   m.def("disable_leak_checker", []() { py::set_leak_warnings(false); });
+
+  nanobind::intrusive_init(
+      [](PyObject *o) noexcept {
+        nanobind::gil_scoped_acquire guard;
+        Py_INCREF(o);
+      },
+      [](PyObject *o) noexcept {
+        nanobind::gil_scoped_acquire guard;
+        Py_DECREF(o);
+      });
 }
 
 }  // namespace python

runtime/bindings/python/vm.h
@@ -7,6 +7,8 @@
 #ifndef IREE_BINDINGS_PYTHON_IREE_RT_VM_H_
 #define IREE_BINDINGS_PYTHON_IREE_RT_VM_H_
 
+#include <nanobind/intrusive/ref.h>
+
 #include <optional>
 
 #include "./binding.h"
@@ -17,6 +19,8 @@
 
 namespace iree {
 namespace python {
+
+class HalModuleDebugSink;
 
 class FunctionAbi;
 
@@ -153,6 +157,9 @@ class VmModule : public ApiRefCounted<VmModule, iree_vm_module_t> {
   }
 
   py::object get_stashed_flatbuffer_blob() { return stashed_flatbuffer_blob; }
+  void SetHalModuleDebugSink(
+      const nanobind::ref<HalModuleDebugSink>& debugSink);
+  const nanobind::ref<HalModuleDebugSink>& GetHalModuleDebugSink() const;
 
  private:
   // If the module was created from a FlatBuffer blob, we stash it here.
@@ -162,6 +169,7 @@ class VmModule : public ApiRefCounted<VmModule, iree_vm_module_t> {
   // vm module), we ensure that there are no dangling references when
   // that deallocator is called.
   py::object stashed_flatbuffer_blob = py::none();
+  nanobind::ref<HalModuleDebugSink> debug_sink;
 };
 
 //------------------------------------------------------------------------------

sogartar/SHARK-Runtime

runtime/bindings/python/initialize_module.cc
@@ -4,7 +4,10 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <nanobind/intrusive/counter.h>
+
 #include <memory>
+#include <nanobind/intrusive/counter.inl>
 
 #include "./binding.h"
 #include "./hal.h"
@@ -78,6 +81,16 @@ NB_MODULE(_runtime, m) {
   });
 
   m.def("disable_leak_checker", []() { py::set_leak_warnings(false); });
+
+  py::intrusive_init(
+      [](PyObject *o) noexcept {
+        py::gil_scoped_acquire guard;
+        Py_INCREF(o);
+      },
+      [](PyObject *o) noexcept {
+        py::gil_scoped_acquire guard;
+        Py_DECREF(o);
+      });
 }
 
 }  // namespace python

runtime/bindings/python/vm.h
@@ -7,6 +7,8 @@
 #ifndef IREE_BINDINGS_PYTHON_IREE_RT_VM_H_
 #define IREE_BINDINGS_PYTHON_IREE_RT_VM_H_
 
+#include <nanobind/intrusive/ref.h>
+
 #include <optional>
 
 #include "./binding.h"
@@ -17,6 +19,8 @@
 
 namespace iree {
 namespace python {
+
+class HalModuleDebugSink;
 
 class FunctionAbi;
 
@@ -153,6 +157,8 @@ class VmModule : public ApiRefCounted<VmModule, iree_vm_module_t> {
   }
 
   py::object get_stashed_flatbuffer_blob() { return stashed_flatbuffer_blob; }
+  void SetHalModuleDebugSink(const py::ref<HalModuleDebugSink>& debug_sink);
+  const py::ref<HalModuleDebugSink>& GetHalModuleDebugSink() const;
 
  private:
   // If the module was created from a FlatBuffer blob, we stash it here.
@@ -162,6 +168,10 @@ class VmModule : public ApiRefCounted<VmModule, iree_vm_module_t> {
   // vm module), we ensure that there are no dangling references when
   // that deallocator is called.
   py::object stashed_flatbuffer_blob = py::none();
+  // Retains a reference to the debug sink as long as the HAL module
+  // Python object lives. We use this to traverse cyclic references.
+  // Where the debug sink callbacks may reference this module.
+  py::ref<HalModuleDebugSink> hal_module_debug_sink;
 };
 
 //------------------------------------------------------------------------------